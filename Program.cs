5c3aef525ca4fc6f6bf079f89b1094a9340bfbb1
Ola Eiman <evileiman@hotmail.com>
2026-08-25 20:59:21 +0200

    300iq try-catch

--- Program.cs
+++ Program.cs
@@ -1,7 +1,69 @@
-﻿namespace ATM;
+﻿using System.Security.Cryptography;
+
+namespace ATM;
 
 class Program {
-    static void Main(string[] args) {
-        Console.WriteLine("Hello, Skibbidi!");
+    
+    //deklaera en variable för aktuellt saldo
+    static int balance = 0;
+    static string amount = "";
+    static void ReportBalance() {
+        //Console.WriteLine($"Du har {balance} kr på ditt konto.");
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine($"Ditt saldo: {balance} kr.\n");
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+    static void Main() {
+
+        while (true) {
+            
+            Console.Clear();
+            ReportBalance();
+            
+            //insättning
+            Console.WriteLine("Hur mycket vill du sätta in?");
+            
+            //deklarera en variable för insättning
+            //var amount = Console.ReadLine(); //tryck enter för fortsättning
+            amount = Console.ReadLine();
+            try {
+                balance += int.Parse(amount);
+                break;
+            }
+            catch {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\nOgiltliga tecken, försök igen...");
+                Console.ForegroundColor = ConsoleColor.White;
+                Thread.Sleep(1000);
+            }
+        }
+/*
+        if (string.IsNullOrWhiteSpace(amount)) {
+            Console.WriteLine("DU MÅSTE ANGE HUR MYCKET DU VILL SÄTTA IN...");
+            Environment.Exit(0);
+        }
+        balance = int.Parse(amount);
+        ReportBalance();
+*/
+
+        Console.WriteLine("Hur mycket vill du ta ut?");
+        amount = Console.ReadLine();
+        Console.WriteLine($"Du vill ta ut {amount} Kr");
+
+        //balance = balance - int.Parse(amount); fult och dåligt sätt
+        balance -= int.Parse(amount);            //snyggt och pro sätt
+
+        ReportBalance();
+
+        /*
+        
+        kontrollera att man inte tar ut 0
+
+        du ska ej kunna bli minus på saldot av uttag
+        eller ta ut mer än vad saldot innehåller
+
+        skriv funktion som visar vad saldot innehåller istället för en massa console lines
+
+        */
     }
 }
